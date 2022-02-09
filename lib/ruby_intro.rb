409f1376ddd7b40eda08e73b8203618c212cff7c
Ubuntu <ubuntu@ip-172-31-14-17.ec2.internal>
2022-02-09 04:04:26 +0000

    Kim_Youngki_HW1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,60 @@
 # Part 1
 
 def sum arr
-   Yffffffc
+  return arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+    b = arr.sort
+    c = b.at(-2)
+    d = b.at(-1)
+    return c.to_i + d.to_i
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.combination(2).any? { |a, b| a + b == n } == true
+    return true
+  else 
+    return false
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  elsif "bcdfghjklmnpqrstvwxyz".include?(s[0].downcase)
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  elsif s.match? /[[:alpha:]]/
+    return false
+  elsif s.to_i % 4 == 0
+    return true
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODEddd HERE
-endddd
+  def initialize isbn, price
+    @isbn = isbn
+    @price =price
+    raise ArgumentError if isbn.length ==0
+    raise ArgumentError if price <= 0
+  end
+  attr_accessor :isbn, :price
+  
+  def price_as_string
+    return "$%0.2f" % @price
+  end
+end
